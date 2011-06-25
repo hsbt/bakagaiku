9ce5f05bbce5086ee01a25a39bed454ed66ab82f
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-06-25 18:05:13 +0900

    upgrade rails-3.1

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source :rubygems
+
+gem 'rails', '~> 3.1.0.rc'
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'coffee-script'
+gem 'uglifier'
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'sqlite3'
+end
+
+group :test do
+  gem 'turn', :require => false
+end
+
+group :prdocution do
+  gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bakagaiku::Application.config.secret_token = '63a1c508185ba13f7fb73f2246f78ef389a9adea600cc43a541f7fbce8b006e22b629313da87cdc551f936dee1aff627067ded6a97354b3359edd452f0907475'
