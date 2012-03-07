f25fc5df11255b2e34af900014e7abea5d29776f
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-07 12:44:25 +0900

    reduce instance value.

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
 
 class EntryController < ApplicationController
-	def index
-		@entries = Entry.limit(5)
-	end
+  def index
+    @entries = Entry.limit(5)
+  end
 
-	def list
-		@entries = Entry.page(params[:page])
-	end
+  def list
+    @entries = Entry.page(params[:page])
+  end
 
-	def show
-		@entry = Entry.where(:bakaid => params[:id]).limit(1).first
-		@pre_entry	= @entry.pre_entry
-		@next_entry = @entry.next_entry
-	end
+  def show
+    @entry = Entry.where(:bakaid => params[:id]).limit(1).first
+  end
 
-	def mview
-		@bakaid = params[:id]
-		@bakaid = '' if @bakaid.size < 6
-		@entries = Entry.find_by_sql ['select * from entries where bakaid like ?', "#{@bakaid}%"]
-	end
+  def mview
+    @bakaid = params[:id]
+    @bakaid = '' if @bakaid.size < 6
+    @entries = Entry.find_by_sql ['select * from entries where bakaid like ?', "#{@bakaid}%"]
+  end
 
-	def feed
+  def feed
     @entries = Entry.limit(5)
 
     respond_to do |format|
