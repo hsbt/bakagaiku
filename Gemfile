c677bb150e8cbbc9fd98a31d088b2dc010612a10
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-03-19 13:32:52 +0900

    upgrade rails4

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.0.0'
 
-gem 'rails'
+gem 'rails', '~> 4.0.0.beta1'
 gem 'haml-rails'
 gem 'jquery-rails'
 gem 'settingslogic'
@@ -9,8 +9,8 @@ gem 'kaminari'
 # gem 'rails_admin'
 
 group :assets do
-  gem 'sass-rails'
-  gem 'coffee-rails'
+  gem 'sass-rails', '~> 4.0.0.beta1'
+  gem 'coffee-rails', '~> 4.0.0.beta1'
   gem 'uglifier'
 end
 
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,11 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bakagaiku::Application.config.secret_token = '6b1e5d62ea5c7521e4b1746f23163be1b6fb6eb98440a97306dd8c23f62a44f3649cf19d96dbe89fcb9a86fdb388e55381ad1e449beffbb8e5ece1cc6fd3bd89'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bakagaiku::Application.config.secret_key_base = '65dde8f12d94d850f3ad28dba629868f02823bb4e6b4f5ffe78b197b2b58562205ef0a6e3c7f72f31388f1aa4f938b075ec6d8dc87bcb48a4f00f401a6b3fbc0'
